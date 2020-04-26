27a90017f1d289527a3776e29f0e4a40d26af9eb
root <valeragg65@gmail.com>
2020-04-26 18:50:20 +0300

    - burger button added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
-import {MatButtonModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
@@ -20,6 +20,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     MatSidenavModule,
     MatButtonModule,
     MatListModule,
+      MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
